611131d983b16d7e9ba7f24fb165fd0f8d0f2c97
Rekseto <ReksetoAjx@gmail.com>
2018-03-30 14:50:17 +0200

    refactoring Inventory

--- core/inventory/Inventory.js
+++ core/inventory/Inventory.js
@@ -1,17 +1,29 @@
 import InventoryError from "./InventoryError";
 
 class Inventory {
-  constructor(baseSize) {
+  constructor(baseSize, baseInv = []) {
     this.size = baseSize;
-    this.inv = [];
+    this.inv = baseInv;
+  }
+
+  increaseSize(size) {
+    this.size = this.size + size;
+  }
+
+  decreaseSize(size) {
+    this.size = this.size - size;
   }
 
   addItem(item) {
     const {size,inv} = this;
     if(inv.length < size) {
       inv.push(item);
+      return `You recived ${item}`;
     } else {
-      return new InventoryError();
+      throw new InventoryError();
     }
   }
 }
+
+
+export default Inventory;
