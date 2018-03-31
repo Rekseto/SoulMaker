d03a9f40c81532bc7859e3d7d8fe7e4869d2b6ce
Rekseto <ReksetoAjx@gmail.com>
2018-04-01 01:58:26 +0200

    MapUtils - Refactor

--- utils/MapUtils.js
+++ utils/MapUtils.js
@@ -1,5 +1,5 @@
-const inRange = (target,el) => {
+const distance = (target,el) => {
  return Math.sqrt(Math.pow(target.x - el.x, 2) + Math.pow(target.y - el.y, 2))
 }
 
-export default {inRange};
+export default {distance};
