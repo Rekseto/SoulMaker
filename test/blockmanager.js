fc8868a7b614542e9033add45f88e83974462682
Rekseto <ReksetoAjx@gmail.com>
2018-04-01 16:15:42 +0200

    BlockManager - tests

--- test/blockmanager.js
+++ test/blockmanager.js
@@ -1,61 +1,59 @@
 import chai from "chai";
-import path from 'path';
+import path from "path";
 
 const should = chai.should();
 const expect = chai.expect;
 const assert = chai.assert;
 const request = chai.request;
 
-import BlockManager from '../core/BlockManager';
+import BlockManager from "../core/BlockManager";
 
-
-describe('BlockManager', function() {
-  describe('#loadBlocks', function() {
-    it('should throw error when wrong path given', function(done) {
-      const blocksPath = path.resolve('./blocksas');
+describe("BlockManager", function () {
+  describe("#loadBlocks", function () {
+    it("should throw error when wrong path given", function (done) {
+      const blocksPath = path.resolve("./blocksas");
 
       const blockManager = new BlockManager();
       blockManager.loadBlocks(blocksPath)
-      .then(res => done("Error has been not occured"))
-      .catch(err => done())
+        .then(res => done("Error has been not occured"))
+        .catch(err => done())
     });
 
-    it('size should increase', function(done) {
-      const blocksPath = path.resolve('./test/blocks');
+    it("size should increase", function (done) {
+      const blocksPath = path.resolve("./test/blocks");
 
       const blockManager = new BlockManager();
       const oldSize = blockManager.blocks.size;
       blockManager.loadBlocks(blocksPath)
-      .then(res => {
-        if(blockManager.blocks.size > oldSize) {
-          done()
-        } else {
-          done(new Error('Size didnt increase'));
-        }
-      })
-      .catch(err => {
-        done(err)
-      });
+        .then(res => {
+          if (blockManager.blocks.size > oldSize) {
+            done()
+          } else {
+            done(new Error("Size didnt increase"));
+          }
+        })
+        .catch(err => {
+          done(err)
+        });
     });
   });
 
-
-  describe('#getBlock' , function() {
+  describe("#getBlock", function () {
     it("should return block which is moveable (GRASS_BLOCK)", function (done) {
-      const blocksPath = path.resolve('./test/blocks');
+      const blocksPath = path.resolve("./test/blocks");
       const blockManager = new BlockManager();
 
       blockManager.loadBlocks(blocksPath)
-      .then(res => {
-        if(blockManager.getBlock(1).moveable === true) {
-          done();
-        } else {
-          done(new Error("Found bad data at block with id 1"));
-        }
-      })
-      .catch(err => {
-        done(err)
-      });
+        .then(res => {
+          if (blockManager.getBlock(1).moveable === true) {
+            done();
+          } else {
+            done(new Error("Found bad data at block with id 1"));
+          }
+        })
+        .catch(err => {
+          done(err)
+        });
     });
   });
 });
