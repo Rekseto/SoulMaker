e6e485cf255754d3aeed2bcfc9ce39039d9bc3ca
Rekseto <ReksetoAjx@gmail.com>
2018-04-02 03:50:54 +0200

    MoveEvent - added target property

--- core/event/events/moveEvent.js
+++ core/event/events/moveEvent.js
@@ -1,9 +1,10 @@
 class MoveEvent {
-  constructor (newX, newY, oldX, oldY) {
+  constructor (newX, newY, oldX, oldY, target) {
     this.newX = newX;
     this.newY = newY;
     this.oldX = oldX;
     this.oldY = oldY;
+    this.target = target;
   }
 }
 
