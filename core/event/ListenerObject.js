885f93338933fa5bd8dcb249b5113bb81d2836de
Rekseto <ReksetoAjx@gmail.com>
2018-04-02 03:51:22 +0200

    ListenerObject - Simply AutoEvents without NPC

---
+++ core/event/ListenerObject.js
@@ -0,0 +1,10 @@
+import {aggregation} from "../../utils/ObjectUtils";
+import Eventable from "./Eventable";
+
+class ObjectListener extends Eventable {
+  constructor () {
+    super();
+  }
+}
+
+export default ObjectListener;
