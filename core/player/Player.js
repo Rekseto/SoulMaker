fc4e3020e07c545794ba475577722f33bfffa050
Rekseto <ReksetoAjx@gmail.com>
2018-04-02 03:48:55 +0200

    Player - Refactor

--- core/player/Player.js
+++ core/player/Player.js
@@ -1,16 +1,11 @@
-import EventEmitter from "events";
-import {aggregation} from "../../utils/ObjetUtils";
+import {aggregation} from "../../utils/ObjectUtils";
 import Character from "../character/Character";
-import Inventory from "../inventory/Inventory";
-import Eventable from "../event/Eventable";
 import ConsoleInterface from "../interface/ConsoleInterface";
 
-class Player extends aggregation(Eventable, Character, ConsoleInterface) {
-  constructor (x, y, direction = "FORWARD", invSize, invContent) {
-    super(x, y, direction, invSize, invContent);
+class Player extends aggregation(Character, ConsoleInterface) {
+  constructor (x, y, region, direction, invSize, invContent) {
+    super(x, y, region, direction, invSize, invContent);
     this.interactionsList = [];
-    this.inventory = new Inventory();
-    this.eventEmitter = new EventEmitter();
     this.activeQuests = [];
     this.completedQuests = [];
   }
