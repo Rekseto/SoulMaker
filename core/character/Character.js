5bcb93a0409c7b81b64acfa2e4495176d6e55cf8
Rekseto <ReksetoAjx@gmail.com>
2018-04-02 03:51:48 +0200

    Character - Refactor

--- core/character/Character.js
+++ core/character/Character.js
@@ -1,13 +1,17 @@
-import {aggregation} from "../../utils/ObjetUtils";
+import {aggregation} from "../../utils/ObjectUtils";
 
 import Moveable from "./Moveable";
 import Eventable from "../event/Eventable";
 import Inventory from "../inventory/Inventory";
 
 class Character extends aggregation(Moveable, Eventable) {
-  constructor (x, y, direction = "FORWARD", invSize, invContent) {
-    super(x, y, direction);
+  constructor (x, y, region, direction, invSize, invContent) {
+    super(x, y, region, direction);
     this.inventory = new Inventory(invSize, invContent);
+  }
+
+  addListener (event, fnc) {
+    this.emitter.on(event, fnc);
   }
 }
 
